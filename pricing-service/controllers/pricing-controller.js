65f745773d59bad470412f75a2820fb8890e1682
Adriana Andronic <adriana.andronic@gmail.com>
2019-08-03 09:39:28 +0200

    remove files no longer used

--- pricing-service/controllers/pricing-controller.js
+++ pricing-service/controllers/pricing-controller.js
@@ -7,16 +7,17 @@ const authMiddleware = require("../middleware/auth");
 
 const pricingService = new PricingService();
 
-router.use([authMiddleware]);
+// router.use([authMiddleware]);
 
-router.get("/:stock", [
-    validator({model: 'Price', scope: 'stock', source: 'params'}),
-], asyncWrapper(async (req, res, next) => {
-    let price = pricingService.getCurrentPrice(req.params.stock);
-    if (!price) {
-        throw new NotFoundError(`Stock ${stock} was not found`);
-    }
-    res.send(price);
-}));
+router.get(
+    "/:stock", [validator({ model: "Price", scope: "stock", source: "params" })],
+    asyncWrapper(async(req, res, next) => {
+        let price = pricingService.getCurrentPrice(req.params.stock);
+        if (!price) {
+            throw new NotFoundError(`Stock ${stock} was not found`);
+        }
+        res.send(price);
+    })
+);
 
 module.exports = router;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const instrumentsController = require('../controllers/instruments');
-
-router
-  .route('/instruments')
-  .get(instrumentsController.getInstruments);
-
-module.exports = router;
